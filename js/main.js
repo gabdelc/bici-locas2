35553ee2d8322710446bef67f98eea7e9c28f992
gabriela <gabdelc@gmail.com>
2017-02-01 15:24:20 -0500

    funcion crear y borrar span completas

--- js/main.js
+++ js/main.js
@@ -1,48 +1,21 @@
-function mensajeNombre()
+
+function crearSpan(inputId, mensaje)
 {
-    var menNombre= document.getElementById("name");
-    var spanNombre=document.createElement("span");
-    spanNombre.innerHTML="No es un nombre válido";
-    menNombre.parentNode.appendChild(spanNombre);
+    var input = document.getElementById(inputId);
+    var nuevoSpan = document.createElement("span");
+    nuevoSpan.innerHTML= mensaje;
+   
+    if(input.nextElementSibling == null){
+        input.parentNode.appendChild(nuevoSpan);
+    }
 }
 
-function mensajeApellido()
+function borrarSpan(inputId)
 {
-    var menApellido= document.getElementById("lastname");
-    var spanApellido = document.createElement("span");
-    spanApellido.innerHTML= "No es un apellido válido";
-    menApellido.parentNode.appendChild(spanApellido);
-}
-
-function mensajeMayuscula()
-{
-   var spanMayuscula = document.createElement("span");
-    spanMayuscula.innerHTML= "Primera letra en mayúscula.";
-    menApellido.parentNode.appendChild(spanMayuscula);
-}
-
-function mensajeEmail()
-{
-    var menMail=document.getElementById("input-email");
-    var spanEmail = document.createElement("span");
-    spanEmail.innerHTML= "No es un correo válido";
-    menMail.parentNode.appendChild(spanEmail);
-}
-
-function mensajeContraseña()
-{
-    var menContraseña = document.getElementById("input-password");
-    var spanContraseña = document.createElement("span");
-    spanContraseña.innerHTML = "No es una contraseña válida.";
-    menContraseña.parentNode.appendChild(spanContraseña);
-}
-
-function mensajeBici()
-{
-    var menBici=document.getElementsByClassName("form-control")[4];
-    var spanBici = document.createElement("span");
-    spanBici.innerHTML= "Escoge una de la lista.";
-    menBici.parentNode.appendChild(spanBici);
+    var input = document.getElementById(inputId);
+    if (input.nextSibling != null){
+     input.parentNode.removeChild(input.nextSibling);
+    }
 }
 
 
@@ -52,7 +25,7 @@ var nombre = document.getElementById("name").value;
 var apellido = document.getElementById("lastname").value;
 var email = document.getElementById("input-email").value;
 var contraseña = document.getElementById("input-password").value;
-//var lista =document.getElementById("bici").selectedIndex;
+var lista =document.getElementById("bici").selectedIndex;
    
      
     if(nombre===""||apellido===""|| email===""|| contraseña==="")
@@ -62,46 +35,47 @@ var contraseña = document.getElementById("input-password").value;
     }
     
     else if (!(/^[a-zA-Z\s]*$/).test(nombre)){
-        mensajeNombre();
+       crearSpan("name", "Los numeros  no estan permitidos");
     }else{
-        console.log("ddddfd");
+        borrarSpan("name");
     }
     
     if(!(/^[a-zA-Z\s]*$/).test(apellido)){
-        mensajeApellido();
+       crearSpan("lastname", "Los numeros  no estan permitidos");
+        
     }else{
-        return false;
-        evt.preventDefault();
+        borrarSpan("lastname");
     }
 
     if (!/\w+@\w+\.+[a-z]/.test(email)) {
-        mensajeEmail();
+       crearSpan("input-email", "E-mail invalido, Ejm. nombre@dominio.com");
+        
     }else{
-        return false;
-        evt.preventDefault();
+        borrarSpan("input-email");
     }
     
     if( contraseña.length < 6 || contraseña === "password"|| contraseña==="123456"|| contraseña==="098754"){
-        mensajeContraseña();
+       crearSpan("input-password", "La ontraseña debe ser mayor a 6 caracteres");
+        
     }else{
-        return false;
-        evt.preventDefault();
+        borrarSpan("input-password");
     }
    
     if( lista == null || lista == 0 ) {
-       mensajeBici();
+        crearSpan("bici", "Elija una opcion de la lista");
+        
     }else{
-        return false;
-        evt.preventDefault();
+        borrarSpan("bici");
     }
     
-    if (nombre.charAt(0) != nombre.charAt(0).toUpperCase()|| apellido.charAt(0) != apellido.charAt(0).toUpperCase())
+   /* if (nombre.charAt(0) != nombre.charAt(0).toUpperCase()|| apellido.charAt(0) != apellido.charAt(0).toUpperCase())
     {
-        mensajeMayuscula();   
+        crearSpan("name", "Primera letra debe ser en mayuscula");
+        
     }else{
-        return false;
-        evt.preventDefault();
+        borrarSpan("name");
     }
+    */
     
   
   
